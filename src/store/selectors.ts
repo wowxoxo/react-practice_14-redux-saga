d82e4d5b295e158011fa47c9b93d880ff3a8fd08
Petr Tsivinsky <petr.tsivinsky@iitrust.ru>
2022-06-27 13:17:33 +0300

    feat: added reselect

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,3 +1,4 @@
+import { createSelector } from "reselect";
 import { AppState } from ".";
 
 export const getUserPostsSelector = (state: AppState) => state.posts.posts;
@@ -5,3 +6,19 @@ export const getUserPostsByQuerySelector = (state: AppState, query = "") =>
   state.posts.posts.filter(
     (post) => post.title.includes(query) || post.body.includes(query)
   );
+
+export const getCounter = (state: AppState) => state.counter;
+export const getPosts = (state: AppState) => state.posts;
+export const getLogin = (state: AppState) => state.login;
+
+export const getUserPostsByQuerySelectorCached = createSelector(
+  getPosts,
+  getLogin,
+  (todos, login) => {
+    if (login.token) {
+      return todos;
+    } else {
+      return [];
+    }
+  }
+);
